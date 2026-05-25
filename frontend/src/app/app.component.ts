b6c162a8471a986e622dbe0fe561e84653bb9909
Shayan Nafees <shayan@Shayan.>
2026-05-25 15:36:02 -0700

    phase 5: Angular frontend with markdown rendering

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -24,4 +24,16 @@ export class AppComponent {
     this.answer = '';
     this.researchService.askStream(this.question, (token)=> this.answer += token, ()=> this.isLoading = false)
   }
+
+  get formattedAnswer(): string {
+    return this.answer
+      .replace(/^### (.+)$/gm, '<h3>$1</h3>')
+      .replace(/^## (.+)$/gm, '<h2>$1</h2>')
+      .replace(/^# (.+)$/gm, '<h1>$1</h1>')
+      .replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
+      .replace(/\*(.+?)\*/g, '<em>$1</em>')
+      .replace(/^- (.+)$/gm, '<li>$1</li>')
+      .replace(/(<li>.*<\/li>)/gs, '<ul>$1</ul>')
+      .replace(/\n/g, '<br>');
+  }
 }
